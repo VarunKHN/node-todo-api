920cdbb3721bc5c0532ffcaac682cbc290cdf1f1
Varun KH <varun.kh@globaldelight.com>
2018-04-17 14:41:28 +0530

    Add Get todos:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 const port = process.env.PORT || 3000;
 var app = express();
@@ -20,7 +21,7 @@ app.post('/todos', (req,res) =>{
     }, (e) =>{
         res.status(400).send(e);
     });
-})
+});
 
 app.get('/todos', (req,res) =>{
     Todo.find().then((todos) =>{
@@ -28,10 +29,23 @@ app.get('/todos', (req,res) =>{
     }, (e) =>{
         res.status(400).send(e);
     });
-})
+});
+
+app.get('/todos/:id',(req,res) =>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) =>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    },(e) => res.status(400).send());
+});
 
 app.listen(port, () =>{
     console.log('Started on port 3000');
-})
+});
 
 module.exports = {app};
